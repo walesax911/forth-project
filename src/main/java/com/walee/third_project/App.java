8fb9b0f1532f39675e04cf922f362d3f50191e55
oyewale olowe <walesax@yahoo.com>
2023-10-12 17:53:03 -0400

    1st commit

---
+++ src/main/java/com/walee/third_project/App.java
@@ -0,0 +1,13 @@
+package com.walee.third_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
